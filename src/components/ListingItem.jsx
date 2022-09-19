aaec311bd99909d20e2bd818aa60d1a3e5a0a27a
kunalb-007 <kunalbhandare422@gmail.com>
2022-09-19 12:28:45 +0530

    Added Delete and Edit functionality to my listing section and completed EditListing page

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
-import { MdLocationOn } from "react-icons/md";
+import { MdLocationOn, MdEdit } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
 
-export default function ListingItem({ listing, id }) {
+export default function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
     <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
       <Link className="contents" to={`/category/${listing.type}/${id}`}>
@@ -22,7 +23,9 @@ export default function ListingItem({ listing, id }) {
         <div className="w-full p-[10px]">
           <div className="flex items-center space-x-1">
             <MdLocationOn className="h-4 w-4 text-green-600" />
-            <p className="font-semibold text-sm mb-[2px] text-gray-600 truncate">{listing.address}</p>
+            <p className="font-semibold text-sm mb-[2px] text-gray-600 truncate">
+              {listing.address}
+            </p>
           </div>
           <p className="font-semibold m-0 text-xl truncate">{listing.name}</p>
           <p className="text-[#457b9d] mt-2 font-semibold">
@@ -52,6 +55,20 @@ export default function ListingItem({ listing, id }) {
           </div>
         </div>
       </Link>
+
+      {onDelete && (
+        <FaTrash
+          className="absolute bottom-2 right-2 h-[14px] cursor-pointer"
+          onClick={() => onDelete(listing.id)}
+        />
+      )}
+
+      {onEdit && (
+        <MdEdit
+          className="absolute bottom-2 right-7 h-4 cursor-pointer"
+          onClick={() => onEdit(listing.id)}
+        />
+      )}
     </li>
   );
 }
